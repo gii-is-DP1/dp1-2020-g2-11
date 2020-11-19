5d6810082d2352707375ab7a80fab15c543c80b7
Jesus <vvjesus32@gmail.com>
2020-11-19 16:56:23 +0100

    Actualizado

---
+++ src/main/java/org/springframework/samples/petclinic/model/Cliente.java
@@ -0,0 +1,93 @@
+/*
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.samples.petclinic.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+
+@MappedSuperclass
+
+@Entity
+@Table(name = "cliente")
+public class Cliente extends BaseEntity {
+
+	@Column(name = "nombre")
+	@NotEmpty
+	protected String nombre;
+
+	@Column(name = "apellidos")
+	@NotEmpty
+	protected String apellidos;
+
+	@Column(name = "dni")
+	@NotEmpty
+	protected String dni;
+	
+	@Column(name = "telefono")
+	@NotEmpty
+	protected String telefono;
+	
+	@Column(name = "email")
+	@NotEmpty
+	protected String email;
+	
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+	
+	
+	
+
+
+}
