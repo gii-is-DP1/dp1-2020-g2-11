a30a36266225d56076acd3ba4dc1fab39889f8a2
Daniel Dorante <dandorsan@alum.us.es>
2020-11-21 20:49:46 +0100

    Interfaz VehiculoRepository

--- src/main/java/org/springframework/samples/petclinic/repository/RevisionRepository.java
+++ src/main/java/org/springframework/samples/petclinic/repository/RevisionRepository.java
@@ -11,7 +11,7 @@ import org.springframework.samples.petclinic.model.Revision;
 public interface RevisionRepository {
 void save (Revision revision) throws DataAccessException;
 
-@Query("SELECT DISTINCT revision FROM Revision revision left join fetch recision.descripcion WHERE revision.fechaRevision:=fechaRevision")
+@Query("SELECT DISTINCT revision FROM Revision revision left join fetch revision.descripcion WHERE revision.fechaRevision:=fechaRevision")
 public Collection<Revision> findByFechaRevision(@Param("fechaRevision") LocalDate fechaRevision);
 
 void delete (Revision revision) throws DataAccessException;
