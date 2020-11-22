823055ab1a61571814bc7d2fcecd7af7a3ef9967
Alejandro Sevillano <Alejandro Sevillano@DESKTOP-RIF95CG>
2020-11-22 16:44:59 +0100

    MecanicoService

---
+++ src/main/java/org/springframework/samples/petclinic/service/MecanicoService.java
@@ -0,0 +1,26 @@
+package org.springframework.samples.petclinic.service;
+
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.samples.petclinic.model.Mecanico;
+import org.springframework.samples.petclinic.repository.MecanicoRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+public class MecanicoService {
+	
+	private MecanicoRepository mecanicoRepository;
+	
+
+	@Autowired
+	public MecanicoService(MecanicoRepository mecanicoRepository) {
+		this.mecanicoRepository = mecanicoRepository; 
+	}	
+	
+	@Transactional(readOnly = true)	
+	public Collection<Mecanico> findMecanico() throws DataAccessException {
+		return mecanicoRepository.findAll();
+	}
+
+}
