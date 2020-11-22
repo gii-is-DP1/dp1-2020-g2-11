5339e0c73daa8313baa554a1b04fbbdcb85b1bef
Alvaro <alvalcgra@alum.us.es>
2020-11-22 13:34:53 +0100

    Factura service

--- src/main/java/org/springframework/samples/petclinic/service/FacturaService.java
+++ src/main/java/org/springframework/samples/petclinic/service/FacturaService.java
@@ -1,7 +1,13 @@
 package org.springframework.samples.petclinic.service;
 
+import java.time.LocalDate;
+import java.util.Collection;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.samples.petclinic.model.Factura;
 import org.springframework.samples.petclinic.repository.FacturaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 public class FacturaService {
 	
@@ -12,5 +18,30 @@ public class FacturaService {
 	public FacturaService(FacturaRepository facturaRepository) {
 		this.facturaRepository = facturaRepository; 
 	}	
+	
+	@Transactional(readOnly = true)
+	public Factura findFacturabyId(Integer id) throws DataAccessException {
+		return facturaRepository.findById(id);
+	}
+	
+	@Transactional(readOnly = true)
+	public Factura findFacturabyFechaEmision(LocalDate fecha) throws DataAccessException {
+		return facturaRepository.findByFechaEmision(fecha);
+	}
+	
+	@Transactional
+	public void saveFactura(Factura factura) throws DataAccessException {
+		facturaRepository.save(factura);
+	}
+	
+	@Transactional
+	public void updateFactura(Factura factura) throws DataAccessException {
+		facturaRepository.update(factura);
+	}
+	
+	@Transactional(readOnly = true)	
+	public Collection<Factura> findFacturas() throws DataAccessException {
+		return facturaRepository.findAll();
+	}
 
 }
