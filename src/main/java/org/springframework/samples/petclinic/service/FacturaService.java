d75471288aa156640f133eed15b3fce43f682257
Eugenio <eugvicvaz@gmail.com>
2020-11-26 20:52:21 +0100

    RN-04

--- src/main/java/org/springframework/samples/petclinic/service/FacturaService.java
+++ src/main/java/org/springframework/samples/petclinic/service/FacturaService.java
@@ -6,40 +6,47 @@ import java.util.Collection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.samples.petclinic.model.Factura;
+import org.springframework.samples.petclinic.model.TipoPago;
 import org.springframework.samples.petclinic.repository.FacturaRepository;
+import org.springframework.samples.petclinic.service.exceptions.TipoPagoException;
 import org.springframework.transaction.annotation.Transactional;
 
 public class FacturaService {
-	
+
 	private FacturaRepository facturaRepository;
-	
 
 	@Autowired
 	public FacturaService(FacturaRepository facturaRepository) {
-		this.facturaRepository = facturaRepository; 
-	}	
-	
+		this.facturaRepository = facturaRepository;
+	}
+
 	@Transactional(readOnly = true)
 	public Factura findFacturabyId(Integer id) throws DataAccessException {
 		return facturaRepository.findById(id);
 	}
-	
+
 	@Transactional(readOnly = true)
 	public Factura findFacturabyFechaEmision(LocalDate fecha) throws DataAccessException {
 		return facturaRepository.findByFechaEmision(fecha);
 	}
-	
+
 	@Transactional
-	public void saveFactura(Factura factura) throws DataAccessException {
-		facturaRepository.save(factura);
+	public void saveFactura(Factura factura) throws DataAccessException, TipoPagoException {
+		TipoPago tpago = factura.getTipoPago();
+		Double precio = factura.getPrecio();
+		if (precio > 300 && tpago == TipoPago.EFECTIVO) {
+			throw new TipoPagoException();
+		} else {
+			facturaRepository.save(factura);
+		}
 	}
-	
+
 	@Transactional
 	public void updateFactura(Factura factura) throws DataAccessException {
 		facturaRepository.update(factura);
 	}
-	
-	@Transactional(readOnly = true)	
+
+	@Transactional(readOnly = true)
 	public Collection<Factura> findFacturas() throws DataAccessException {
 		return facturaRepository.findAll();
 	}
