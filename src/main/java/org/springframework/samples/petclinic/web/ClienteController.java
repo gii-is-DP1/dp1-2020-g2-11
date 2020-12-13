e36c202e6603ee94d2ce3bc989a950251330af5e
eugvicvaz <eugvicvaz@alum.us.es>
2020-12-13 12:56:53 +0100

    ClienteController y meter carpeta de vistas "cliente" en Security

---
+++ src/main/java/org/springframework/samples/petclinic/web/ClienteController.java
@@ -0,0 +1,28 @@
+package org.springframework.samples.petclinic.web;
+
+import java.util.Collection;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.samples.petclinic.model.Cliente;
+import org.springframework.samples.petclinic.service.ClienteService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ClienteController {
+	
+	private ClienteService clienteService;
+	
+	@Autowired
+	public ClienteController(ClienteService clienteService) {
+		this.clienteService = clienteService;
+	}
+	
+	@GetMapping(value= {"/clientes"})
+	public String findAllClientes(Map<String, Object> model) {
+		Collection<Cliente> clientes = clienteService.findClientes();
+		model.put("selections", clientes);
+		return "cliente/ListaClientes";
+	}
+}
