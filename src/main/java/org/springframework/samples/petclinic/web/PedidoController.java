33a9e65b2d020da22329c58c5411dd6327e4da7e
Jesus <vvjesus32@gmail.com>
2020-12-23 12:19:30 +0100

    Cambio pedido controller

--- src/main/java/org/springframework/samples/petclinic/web/PedidoController.java
+++ src/main/java/org/springframework/samples/petclinic/web/PedidoController.java
@@ -6,8 +6,10 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.samples.petclinic.model.Pedido;
 import org.springframework.samples.petclinic.service.ProveedorService;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class PedidoController {
 	
 	private ProveedorService proveedorService;
