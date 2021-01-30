7f600623116e25fc1c81b959066e316b59a61601
Alvaro <alvalcgra@alum.us.es>
2021-01-30 12:13:52 +0100

    Arreglo test proveedor

--- src/test/java/org/springframework/samples/petclinic/service/ProveedorServiceTest.java
+++ src/test/java/org/springframework/samples/petclinic/service/ProveedorServiceTest.java
@@ -4,11 +4,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Collection;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.dao.DataAccessException;
+import org.springframework.samples.petclinic.model.Mecanico;
 import org.springframework.samples.petclinic.model.Proveedor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -57,14 +58,7 @@ public class ProveedorServiceTest {
 	@Transactional
 	public void shoulFindAllProveedores() {
 
-		proveedor = new Proveedor();
-		proveedor.setNombre("Aceites Pedro");
-		proveedor.setTelefono("655442326");
-		proveedor.setDireccion("C/Guadalquivie");
-		proveedor.setEmail("aceitespedro@gmail.com");         
-                
-		proveedorService.saveProveedor(proveedor);
-		Collection<Proveedor> proveedores = this.proveedorService.findProveedorByNombre("Aceites Juan");
+		Collection<Proveedor> proveedores = this.proveedorService.findProveedores();
 		assertThat(proveedores.size()).isEqualTo(2);
 	}
 	
@@ -72,16 +66,9 @@ public class ProveedorServiceTest {
 	@Transactional
 	public void shoulDeleteProveedores() {
 
-		proveedor = new Proveedor();
-		proveedor.setId(1);
-		proveedor.setNombre("Neumaticos Paco");
-		proveedor.setTelefono("655442233");
-		proveedor.setDireccion("C/Reina Mercedes");
-		proveedor.setEmail("neumaticosPaco@gmail.com");         
-                
-		proveedorService.saveProveedor(proveedor);
-		proveedorService.deleteProveedor(proveedor.getId());
-		assertThat(proveedorService.findProveedorByNombre("Neumaticos Paco")).isNull();
+		proveedorService.deleteProveedor(2);
+        Collection<Proveedor> proveedores = this.proveedorService.findProveedores();
+        assertThat(proveedores.size()).isEqualTo(1);
 		
 	}
 
