22e98644dd32de013ffab4d8d2bbf0771e7eb4ac
Alejandro Sevillano <Alejandro Sevillano@DESKTOP-RIF95CG>
2021-01-31 11:38:25 +0100

    VehiculoService y VehiculoServiceTest

---
+++ src/main/java/org/springframework/samples/petclinic/service/VehiculoService.java
@@ -0,0 +1,60 @@
+package org.springframework.samples.petclinic.service;
+
+import java.time.LocalDate;
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.samples.petclinic.model.TipoVehiculo;
+import org.springframework.samples.petclinic.model.Vehiculo;
+import org.springframework.samples.petclinic.repository.VehiculoRepository;
+import org.springframework.samples.petclinic.service.exceptions.VehiculosAntiguo;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+	@Service
+	public class VehiculoService {
+
+		private VehiculoRepository vehiculoRepository;
+
+		@Autowired
+		public VehiculoService(VehiculoRepository vehiculoRepository) {
+		this.vehiculoRepository = vehiculoRepository;
+		}
+
+
+		@Transactional(readOnly = true)
+		public Collection<Vehiculo> findVehículoByMatricula(String matricula) throws DataAccessException {
+			return vehiculoRepository.findByMatricula(matricula);
+		}
+
+		@Transactional(readOnly = true)
+		public Collection<Vehiculo> findVehículoByTipo(TipoVehiculo tipo) throws DataAccessException {
+			return vehiculoRepository.findByTipoVehiculo(tipo);
+		}
+
+		@Transactional(readOnly = true)
+		public Collection<Vehiculo> findVehiculos() throws DataAccessException {
+			return (Collection<Vehiculo>) vehiculoRepository.findAll();
+		}
+
+		@Transactional(rollbackFor = VehiculosAntiguo.class)
+		public void saveVehiculo(Vehiculo vehiculo) throws DataAccessException, VehiculosAntiguo {
+			if (vehiculo.getFechaFabricacion().getYear() - LocalDate.now().getYear() < 12) {
+				vehiculoRepository.save(vehiculo);
+			} else {
+				throw new VehiculosAntiguo();
+			}
+		}
+
+//		@Transactional
+//		public void deleteVehiculo(String matricula) throws DataAccessException {
+//			vehiculoRepository.delete(matricula);
+//		}
+
+		public void deleteVehiculo(Integer id) {
+			vehiculoRepository.deleteById(id);
+		}
+	}
+
+
+
