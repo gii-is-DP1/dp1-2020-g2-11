1f070d7f948b77df0382c20a350532bb029f7343
USER-PC <USER-PC@DESKTOP-BHI57B9>
2021-02-01 12:44:17 +0100

    Vehiculo Controller Completado

--- src/main/java/org/springframework/samples/petclinic/web/VehiculoController.java
+++ src/main/java/org/springframework/samples/petclinic/web/VehiculoController.java
@@ -1,11 +1,14 @@
 package org.springframework.samples.petclinic.web;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.samples.petclinic.service.AuthoritiesService;
 
 import org.springframework.samples.petclinic.service.VehiculoService;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class VehiculoController {
@@ -16,5 +19,9 @@ public class VehiculoController {
 	public VehiculoController(VehiculoService vehiculoService, AuthoritiesService authoritiesService) {
 		this.vehiculoService = vehiculoService;
 	}
-
+@GetMapping(value={""})
+public String findAllVehiculos(Map<String, Object> model) {
+	Collection<Vehiculo> vehiculos = vehiculo
 }
+}
+
