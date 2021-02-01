c9f60ea049d2c85d0161bd02cf6731cafb5320ef
Alvaro <alvalcgra@alum.us.es>
2021-02-01 11:23:10 +0100

    Arreglo de estanciaController

--- src/main/java/org/springframework/samples/petclinic/web/EstanciaController.java
+++ src/main/java/org/springframework/samples/petclinic/web/EstanciaController.java
@@ -5,23 +5,24 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.samples.petclinic.model.Estancia;
-import org.springframework.samples.petclinic.service.CitaService;
+import org.springframework.samples.petclinic.service.EstanciaService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class EstanciaController {
-private CitaService citaService;
+	private EstanciaService estanciaService;
 
-@Autowired
-public EstanciaController(CitaService citaService) {
-	this.citaService=citaService;
-}
-@GetMapping(value= {"/estancias"} )
-public String findAllEstancias(Map<String, Object> model){
-	Collection<Estancia> estancias= citaService.findEstanciasActuales();
-	model.put("selections", estancias);
-	return "estancias/ListaEstancias";
-}
+	@Autowired
+	public EstanciaController(EstanciaService citaService) {
+		this.estanciaService = estanciaService;
+	}
+
+	@GetMapping(value = { "/estancias" })
+	public String findAllEstancias(Map<String, Object> model) {
+		Collection<Estancia> estancias = estanciaService.findEstanciasActuales();
+		model.put("selections", estancias);
+		return "estancias/ListaEstancias";
+	}
 
 }
