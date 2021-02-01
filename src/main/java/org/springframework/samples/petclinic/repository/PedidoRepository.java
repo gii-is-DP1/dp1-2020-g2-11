a851cca4ceb85c278e60959ff5d91110967dcb2b
Jesus <vvjesus32@gmail.com>
2021-02-01 19:28:43 +0100

    ProveedorService corregido

--- src/main/java/org/springframework/samples/petclinic/repository/PedidoRepository.java
+++ src/main/java/org/springframework/samples/petclinic/repository/PedidoRepository.java
@@ -2,23 +2,15 @@ package org.springframework.samples.petclinic.repository;
 
 import java.time.LocalDate;
 import java.util.Collection;
-import java.util.Optional;
 
-import org.springframework.dao.DataAccessException;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.Repository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.samples.petclinic.model.Pedido;
 
-public interface PedidoRepository extends Repository<Pedido, Integer> {
-	void save(Pedido pedido) throws DataAccessException;
+public interface PedidoRepository extends CrudRepository<Pedido, Integer> {
 
 	@Query("SELECT pedido FROM Pedido pedido WHERE pedido.fechaEntrada =:fechaEntrada")
 	public Collection<Pedido> findByFechaPedido(@Param("fechaEntrada") LocalDate fechaEntrada);
 	
-	@Query("SELECT pedido FROM Pedido pedido WHERE pedido.id =:id")
-	public Optional<Pedido> findById(@Param("id") int id);
-	
-	Collection<Pedido> findAll() throws DataAccessException;
-
 }
