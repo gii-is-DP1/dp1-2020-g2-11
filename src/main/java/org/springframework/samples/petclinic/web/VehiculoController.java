d304d183078a09ebcd8084121a86c3efeed76c94
Jesus <vvjesus32@gmail.com>
2021-02-02 11:38:52 +0100

    VehiculoControllerTest semifuncionando

--- src/main/java/org/springframework/samples/petclinic/web/VehiculoController.java
+++ src/main/java/org/springframework/samples/petclinic/web/VehiculoController.java
@@ -1,7 +1,6 @@
 package org.springframework.samples.petclinic.web;
 
 import java.util.Collection;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.samples.petclinic.model.TipoVehiculo;
@@ -9,70 +8,67 @@ import org.springframework.samples.petclinic.model.Vehiculo;
 import org.springframework.samples.petclinic.service.AuthoritiesService;
 import org.springframework.samples.petclinic.service.VehiculoService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class VehiculoController {
-	
+
 	private VehiculoService vehiculoService;
-	
+
 	@Autowired
 	public VehiculoController(VehiculoService vehiculoService, AuthoritiesService authoritiesService) {
 		this.vehiculoService = vehiculoService;
 	}
-@GetMapping(value={"/vehiculos"})
-public String findAllVehiculos(Map<String, Object> model) {
-	Collection<Vehiculo> vehiculos = vehiculoService.findVehiculos();
-	model.put("selections", vehiculos);
-	return"vehiculos/ListaVehiculos";
-}
 
-@GetMapping(value= {"/vehiculos"})
-public String findVehiculoByTipo(String tpo ,BindingResult res,Map<String, Object> model) {
-	
-	if(tpo==null||tpo=="") { 
+	@GetMapping(value = { "/vehiculos" })
+	public String findAllVehiculos(ModelMap model) {
 		Collection<Vehiculo> vehiculos = vehiculoService.findVehiculos();
-		model.put("selections", vehiculos);
+		model.put("vehiculos", vehiculos);
+		return "vehiculos/ListaVehiculos";
 	}
-	else {
-	TipoVehiculo tp= TipoVehiculo.valueOf(tpo);
-	Collection<Vehiculo> results=vehiculoService.findVehículoByTipo(tp);
-	if(results.isEmpty()) {
-		res.rejectValue("tipoVehiculo","notFound",  "not found");
+
+	@GetMapping(value = { "/vehiculo/tipo" })
+	public String findVehiculoByTipo(String tpo, BindingResult res, ModelMap model) {
+
+		if (tpo == null || tpo == "") {
+			Collection<Vehiculo> vehiculos = vehiculoService.findVehiculos();
+			model.put("vehiculoTipo", vehiculos);
+		} else {
+			TipoVehiculo tp = TipoVehiculo.valueOf(tpo);
+			Collection<Vehiculo> results = vehiculoService.findVehículoByTipo(tp);
+			if (results.isEmpty()) {
+				res.rejectValue("tipoVehiculo", "notFound", "not found");
+			} else {
+				model.put("vehiculoTipo", results);
+			}
+		}
+		return "proveedor/ListaVehiculos";
+
 	}
-	else {
-		model.put("selections", results);
-	}
-	}
-	return "proveedor/ListaVehiculos";
-	
-}
-@GetMapping(value= {"/vehiculos"})
-public String findVEhiculosByMatricula(String matricula,BindingResult res, Map<String, Object> model) {
-	
-	if(matricula==null) { 
-	Collection<Vehiculo> vehiculos = vehiculoService.findVehiculos();
-	model.put("selections", vehiculos);
-	return "vehiculo/listaVehiculos";
-	}
-	else {
-		Collection<Vehiculo> results=vehiculoService.findVehículoByMatricula(matricula);
-		if (results.isEmpty()) {
-			res.rejectValue("matricula","notFound",  "not found");
+
+	@GetMapping(value = { "/vehiculo" })
+	public String findVehiculosByMatricula(String matricula, BindingResult res, ModelMap model) {
+
+		if (matricula == null) {
+			Collection<Vehiculo> vehiculos = vehiculoService.findVehiculos();
+			model.put("vehiculoMat", vehiculos);
 			return "vehiculo/listaVehiculos";
+		} else {
+			Collection<Vehiculo> results = vehiculoService.findVehículoByMatricula(matricula);
+			if (results.isEmpty()) {
+				res.rejectValue("matricula", "notFound", "not found");
+				return "vehiculo/listaVehiculos";
+			} else if (results.size() == 1) {
+				Vehiculo vehiculo = results.iterator().next();
+				return "redirect:/vehiculo/" + vehiculo.getMatricula();
+			} else {
+				model.put("selections", results);
+				return "vehiculo/listaVehiculos";
+			}
 		}
-		else if (results.size()==1){
-			Vehiculo vehiculo= results.iterator().next();
-			return "redirect:/vehiculo/"+vehiculo.getMatricula();
-		}
-		else {
-			model.put("selections", results);
-			return "vehiculo/listaVehiculos";
-		}
+
 	}
 
 }
-
-}
-
