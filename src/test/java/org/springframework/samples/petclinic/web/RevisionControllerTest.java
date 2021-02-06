5266843182e2ef79bd40063a95c9aa3365db1573
Alvaro <alvalcgra@alum.us.es>
2021-02-06 20:10:13 +0100

    Revision Controller Test

--- src/test/java/org/springframework/samples/petclinic/web/RevisionControllerTest.java
+++ src/test/java/org/springframework/samples/petclinic/web/RevisionControllerTest.java
@@ -7,7 +7,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import java.time.LocalDate;
 
-import org.assertj.core.util.Lists;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,7 +17,9 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.samples.petclinic.configuration.SecurityConfiguration;
 import org.springframework.samples.petclinic.model.Revision;
+import org.springframework.samples.petclinic.service.ClienteService;
 import org.springframework.samples.petclinic.service.RevisionService;
+import org.springframework.samples.petclinic.service.VehiculoService;
 import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
@@ -30,6 +32,10 @@ public class RevisionControllerTest {
 
 	@MockBean
 	private RevisionService revisionService;
+	@MockBean
+	private ClienteService clienteService;
+	@MockBean
+	private VehiculoService vehiculoService;
 
 	@Autowired
 	private MockMvc mockMvc;
@@ -37,7 +43,7 @@ public class RevisionControllerTest {
 	@BeforeEach
 	void setup() {
 		Revision revision = new Revision();
-		revision.setId(1);
+		revision.setId(5);
 		revision.setDescripcion("Ruedas traseras y frenos desgastados");
 		revision.setDuracion(5);
 		revision.setFechaRevision(LocalDate.of(2021, 1, 23));
@@ -49,15 +55,24 @@ public class RevisionControllerTest {
 	@WithMockUser(value = "spring")
 	@Test
 	void findAllRevisionesTest() throws Exception {
-		mockMvc.perform(get("/revisiones")).andExpect(status().isOk()).andExpect(model().attributeExists("revisiones"))
-				.andExpect(view().name("revisiones/Revisiones"));
+		mockMvc.perform(get("/revisiones")).andExpect(status().isOk())
+		.andExpect(model().attributeExists("revisiones"))
+		.andExpect(view().name("revisiones/ListaRevisiones"));
 	}
 
 	@WithMockUser(value = "spring")
 	@Test
 	void findRevisionByFechaTest() throws Exception {
 		mockMvc.perform(get("/revisionFecha")).andExpect(status().isOk())
-				.andExpect(model().attributeExists("revisiones"))
-				.andExpect(view().name("revisiones/Revisiones"));
+			.andExpect(model().attributeExists("revisiones"))
+			.andExpect(view().name("revisiones/ListaRevisiones"));
 	}
+	
+	 @WithMockUser(value = "spring")
+	 @Test
+	 void findRevisionById() throws Exception{
+		 mockMvc.perform(get("/revision/{revisionId}",5)).andExpect(status().isOk())
+		// .andExpect(model().attributeExists("revision"));
+		 .andExpect(view().name("revisiones/revisionDetails"));
+	 }
 }
