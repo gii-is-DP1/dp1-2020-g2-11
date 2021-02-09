abbb080dcb6f369c01d0a263b7af50fcaf7b422d
Alvaro <alvalcgra@alum.us.es>
2021-02-09 17:33:40 +0100

    RevisionValidator

--- src/main/java/org/springframework/samples/petclinic/web/RevisionValidator.java
+++ src/main/java/org/springframework/samples/petclinic/web/RevisionValidator.java
@@ -12,10 +12,29 @@ public class RevisionValidator implements Validator {
 
 		Revision revision = (Revision) obj;
 		LocalDate fechaRevision = revision.getFechaRevision();
-
-		if (fechaRevision.isAfter(LocalDate.now())) {
+		String descripcion = revision.getDescripcion();
+		Integer duracion = revision.getDuracion();
+		String cliente = revision.getCliente().getDni();
+		
+		
+		if(fechaRevision == null) {
+			errors.rejectValue("fechaRevision", "este campo no puede estar vacio", "este campo no puede estar vacio");
+		}else if(fechaRevision.isAfter(LocalDate.now())) {
 			errors.rejectValue("fechaRevision", "la fecha de revisión debe ser anterior a la fecha actual", "la fecha de revisión debe ser anterior a la fecha actual");
 		}
+		
+		if(descripcion == null) {
+			errors.rejectValue("descripcion", "este campo no puede estar vacio", "este campo no puede estar vacio");
+		}
+		if(duracion == null) {
+			errors.rejectValue("duracion", "este campo no puede estar vacio", "este campo no puede estar vacio");
+		}
+		
+		if(cliente == null) {
+			errors.rejectValue("cliente", "este campo no puede estar vacio", "este campo no puede estar vacio");
+		}
+		
+		
 	}
 
 	@Override
